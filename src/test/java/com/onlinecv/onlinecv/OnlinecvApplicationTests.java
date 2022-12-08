b477a126088af9f8a09cdd2c378831613aadcfe9
Vyalkuran <alexsebastian2006@yahoo.com>
2022-12-08 12:59:01 +0200

    Initial Commit

---
+++ src/test/java/com/onlinecv/onlinecv/OnlinecvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinecv.onlinecv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
